8676488c758f4084ded289320aa0dd7b51bdd035
Chad Shaver <c256s856@home.ku.edu>
2019-09-19 10:59:12 -0600

    load initial database tables. Create post deploy folders and scripts.

---
+++ Post_Deploy/Test.sql
@@ -0,0 +1,1 @@
+﻿  UPDATE Properties set Owners ='XXX'
---
+++ dbo/Tables/Properties.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[Properties] (
+    [Property_ID]    INT          IDENTITY (1, 1) NOT NULL,
+    [Street]         VARCHAR (50) NOT NULL,
+    [City]           VARCHAR (50) NOT NULL,
+    [State]          VARCHAR (50) NOT NULL,
+    [ZIP]            VARCHAR (50) NULL,
+    [Owners]         VARCHAR (50) NULL,
+    [Number_Units]   INT          NOT NULL,
+    [Date_Purchase]  DATE         NOT NULL,
+    [Price_Purchase] MONEY        NOT NULL,
+    [Seller]         VARCHAR (50) NOT NULL,
+    CONSTRAINT [PK_Properties] PRIMARY KEY CLUSTERED ([Property_ID] ASC)
+);
+
