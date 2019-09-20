8294741d72bdfecbaea98532dd3c110f1a730bdc
Chad M Shaver <55406809+shaverRentals@users.noreply.github.com>
2019-09-19 19:05:39 -0600

    creates property related tables. Adds Image field to property table

--- dbo/Tables/Properties.sql
+++ dbo/Tables/Properties.sql
@@ -1,14 +1,18 @@
 ﻿CREATE TABLE [dbo].[Properties] (
-    [Property_ID]    INT          IDENTITY (1, 1) NOT NULL,
-    [Street]         VARCHAR (50) NOT NULL,
-    [City]           VARCHAR (50) NOT NULL,
-    [State]          VARCHAR (50) NOT NULL,
-    [ZIP]            VARCHAR (50) NULL,
-    [Owners]         VARCHAR (50) NULL,
-    [Number_Units]   INT          NOT NULL,
-    [Date_Purchase]  DATE         NOT NULL,
-    [Price_Purchase] MONEY        NOT NULL,
-    [Seller]         VARCHAR (50) NOT NULL,
-    CONSTRAINT [PK_Properties] PRIMARY KEY CLUSTERED ([Property_ID] ASC)
+    [Property_ID]       INT          IDENTITY (1, 1) NOT NULL,
+    [Street]            VARCHAR (50) NOT NULL,
+    [City]              VARCHAR (50) NOT NULL,
+    [State]             VARCHAR (50) NOT NULL,
+    [ZIP]               VARCHAR (50) NULL,
+    [Property_Owner_ID] INT          NULL,
+    [Number_Units]      INT          NOT NULL,
+    [Date_Purchase]     DATE         NOT NULL,
+    [Price_Purchase]    MONEY        NOT NULL,
+    [Seller]            VARCHAR (50) NOT NULL,
+    [Property_Primary_Image] IMAGE NULL, 
+    CONSTRAINT [PK_Properties] PRIMARY KEY CLUSTERED ([Property_ID] ASC),
+    CONSTRAINT [FK_Properties_Property_Owner] FOREIGN KEY ([Property_Owner_ID]) REFERENCES [dbo].[Property_Owner] ([Property_Owner_ID])
 );
+
+
 
---
+++ dbo/Tables/Property_Owner.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Property_Owner] (
+    [Property_Owner_ID]          INT           IDENTITY (1, 1) NOT NULL,
+    [Property_Owner_Description] VARCHAR (244) NULL,
+    CONSTRAINT [PK_Property_Owner] PRIMARY KEY CLUSTERED ([Property_Owner_ID] ASC)
+);
+
