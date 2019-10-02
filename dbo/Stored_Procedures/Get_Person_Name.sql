5a2dbc9bd9e5baeb8b55abecbe61e1918f38868d
Chad Shaver <c256s856@home.ku.edu>
2019-10-02 17:56:49 -0600

    SP for retrieving person name in lease form.

---
+++ dbo/Stored_Procedures/Get_Person_Name.sql
@@ -0,0 +1,15 @@
+﻿CREATE PROCEDURE [dbo].[Get_Person_Name]
+	
+AS
+	
+select
+ Person_ID
+ ,Person_Name_Combined
+FROM People
+UNION
+select
+ 10000
+ ,'Default, Tennant'
+FROM People 
+order by Person_Name_Combined
+;
