9da1cb23d11b9e5a0c502e512ee97404234df6e0
Chad M Shaver <55406809+shaverRentals@users.noreply.github.com>
2019-11-10 19:16:03 -0700

    Iteration/5.0: initial setup for people table (#4)
    * new combo fields for app lease dropdowns
    
    * SP for retrieving person name in lease form.

---
+++ Post_Deploy/Iterations/5.0/Insert_Version.sql
@@ -0,0 +1,17 @@
+﻿DELETE FROM tV
+FROM dbo.Version tV
+WHERE 
+	tV.number_Version = '5.0' 
+	
+
+INSERT INTO dbo.Version (
+	Number_Version
+	,Date_Version
+	,Desc_Version
+	)
+VALUES (
+	'2.0'
+	,GETDATE()
+	,'Lease objects'
+	)
+;
