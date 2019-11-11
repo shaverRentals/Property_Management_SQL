20d6e91f4c41f7965c97c2bcfedeeba4498e5db7
Chad Shaver <c256s856@home.ku.edu>
2019-11-10 19:40:17 -0700

    create person role table

---
+++ dbo/Tables/Person_Role.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Person_Role]
+(
+	[Person_Role_ID] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Person_ID] INT NOT NULL, 
+    [Person_Role] VARCHAR(244) NULL, 
+    CONSTRAINT [FK_Person_Role_ToTable] FOREIGN KEY ([Person_ID]) REFERENCES [People]([Person_ID])
+)
