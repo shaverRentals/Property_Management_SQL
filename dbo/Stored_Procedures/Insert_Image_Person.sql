71abc229eb3c68de537eb6aa063561d373d2a129
Chad Shaver <c256s856@home.ku.edu>
2020-02-17 16:30:35 -0700

    7.0 release set-up. Insert person image stored procedure.

---
+++ dbo/Stored_Procedures/Insert_Image_Person.sql
@@ -0,0 +1,11 @@
+﻿CREATE PROCEDURE [dbo].[Insert_Image_Person]
+
+	@Path varchar(2000)
+	,@Current_ID VARCHAR(50)
+AS
+
+DECLARE @New_Pic Varchar(8000)
+SET @New_Pic = 'UPDATE People SET Person_Image = BulkColumn FROM OPENROWSET(BULK ' + @Path + ', SINGLE_BLOB) image where Person_ID = '  + @Current_ID 
+;
+EXEC(@New_Pic)
+;
