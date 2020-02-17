b71c10110c5a665f26713094e9aaa8918c4baad0
Chad M Shaver <55406809+shaverRentals@users.noreply.github.com>
2020-02-17 16:31:41 -0700

    7.0 release set-up. Insert person image stored procedure. (#6)

---
+++ Post_Deploy/Iterations/7.0/Insert_Version.sql
@@ -0,0 +1,17 @@
+﻿DELETE FROM tV
+FROM dbo.Version tV
+WHERE 
+	tV.number_Version = '7.0' 
+	
+
+INSERT INTO dbo.Version (
+	Number_Version
+	,Date_Version
+	,Desc_Version
+	)
+VALUES (
+	'7.0'
+	,GETDATE()
+	,'Person Image Procedure'
+	)
+;
--- Post_Deploy/Script.PostDeployment.sql
+++ Post_Deploy/Script.PostDeployment.sql
@@ -1,5 +1,8 @@
 ﻿
 --6.0
-:r .\iterations\6.0\insert_Version.sql
-:r .\iterations\6.0\Insert_person_role_type.sql
-:r .\iterations\6.0\Insert_person_status.sql
+--:r .\iterations\6.0\insert_Version.sql
+--:r .\iterations\6.0\Insert_person_role_type.sql
+--:r .\iterations\6.0\Insert_person_status.sql
+
+--7.0
+:r .\iterations\7.0\insert_Version.sql
