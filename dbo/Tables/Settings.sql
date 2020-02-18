df920a8ee34cf6e8eb168b279f5c4281fe75cc3e
Chad Shaver <c256s856@home.ku.edu>
2020-02-18 07:44:54 -0700

    Creates settings table

---
+++ dbo/Tables/Settings.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Settings]
+(
+[Site_Settings_ID] [int] IDENTITY(1,1) NOT NULL,
+	[Setting] [varchar](100) NOT NULL,
+	[Setting_Value] [varchar](max) NULL,
+	[Setting_Description] [varchar](max) NULL, 
+    CONSTRAINT [PK_Settings] PRIMARY KEY ([Site_Settings_ID])
+)
