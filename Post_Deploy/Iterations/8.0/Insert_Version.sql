0107142e10aa7d1b28d91884bae3e40983aaef35
Chad Shaver <c256s856@home.ku.edu>
2020-02-18 07:35:31 -0700

    set up iteration 8.0

---
+++ Post_Deploy/Iterations/8.0/Insert_Version.sql
@@ -0,0 +1,17 @@
+﻿DELETE FROM tV
+FROM dbo.Version tV
+WHERE 
+	tV.number_Version = '8.0' 
+	
+
+INSERT INTO dbo.Version (
+	Number_Version
+	,Date_Version
+	,Desc_Version
+	)
+VALUES (
+	'8.0'
+	,GETDATE()
+	,'Settings table and values'
+	)
+;
--- Post_Deploy/Script.PostDeployment.sql
+++ Post_Deploy/Script.PostDeployment.sql
@@ -4,5 +4,9 @@
 --:r .\iterations\6.0\Insert_person_role_type.sql
 --:r .\iterations\6.0\Insert_person_status.sql
 
---7.0
-:r .\iterations\7.0\insert_Version.sql
+----7.0
+--:r .\iterations\7.0\insert_Version.sql
+
+--8.0
+:r .\iterations\8.0\insert_Version.sql
+
