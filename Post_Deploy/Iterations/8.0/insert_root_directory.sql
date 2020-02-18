f845fc438eb247ae6b3114c562b52751a4e77c46
Chad Shaver <c256s856@home.ku.edu>
2020-02-18 07:58:23 -0700

    inserts settings for root directory

---
+++ Post_Deploy/Iterations/8.0/insert_root_directory.sql
@@ -0,0 +1,13 @@
+﻿INSERT INTO Settings
+(
+ Setting
+ ,Setting_Value
+ ,Setting_Description
+)
+VALUES
+(
+ 'Root_Directory'
+ ,'C:\Users\shave\OneDrive\Documents\Property_Management\Properties'
+ ,'Root directory for storing property management related documents'
+)
+;
--- Post_Deploy/Script.PostDeployment.sql
+++ Post_Deploy/Script.PostDeployment.sql
@@ -4,9 +4,9 @@
 --:r .\iterations\6.0\Insert_person_role_type.sql
 --:r .\iterations\6.0\Insert_person_status.sql
 
-----7.0
+--7.0
 --:r .\iterations\7.0\insert_Version.sql
 
 --8.0
 :r .\iterations\8.0\insert_Version.sql
-
+:r .\iterations\8.0\insert_root_directory.sql
