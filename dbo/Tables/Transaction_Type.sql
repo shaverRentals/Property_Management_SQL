f2b117e1b18bc9f11c57e8b3b4a9797e44668a27
Chad Shaver <c256s856@home.ku.edu>
2020-11-08 08:02:58 -0700

    Creates transaction type table

---
+++ dbo/Tables/Transaction_Type.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[Transaction_Type]
+(
+	[Transaction_Type_ID] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Transaction_Type] VARCHAR(50) NULL
+)
--- dbo/Tables/Transactions.sql
+++ dbo/Tables/Transactions.sql
@@ -1,5 +1,6 @@
 ﻿CREATE TABLE [dbo].[Transactions]
 (
-[Transaction_ID] INT NOT NULL PRIMARY KEY IDENTITY
+[Transaction_ID] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Transaction_Type_ID] INT NULL
 
 )
