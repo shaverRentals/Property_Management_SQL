0dec91984b7b87741eb1ee2762dddf2fd0f40324
Chad M Shaver <55406809+shaverRentals@users.noreply.github.com>
2020-11-07 17:56:26 -0700

    8.0: Documents management (#7)
    * set up iteration 8.0
    
    * Creates settings table
    
    * inserts settings for root directory
    
    * lease path fields and lease folder settings.
    
    * scripts for modifying property descriptions and adding new default folder settings
    
    * add values for default folders
    
    Co-authored-by: Chad Shaver <c256s856@home.ku.edu>

---
+++ Post_Deploy/Iterations/8.0/insert_root_directory.sql
@@ -0,0 +1,41 @@
+﻿DELETE FROM SETTINGS WHERE SETTING IN('Root_Directory','Default_Folder')
+;
+
+INSERT INTO Settings
+(
+ Setting
+ ,Setting_Value
+ ,Setting_Description
+)
+VALUES
+(
+ 'Root_Directory'
+ ,'C:\Users\shave\OneDrive\Documents\Property_Management\Properties'
+ ,'Root directory for storing property management related documents'
+)
+,
+(
+ 'Default_Folder'
+ ,'Leases'
+ ,'Name used for lease folder in the root directory'
+)
+,
+(
+ 'Default_Folder'
+ ,'Docs'
+ ,'Name used for Docs folder in the root directory'
+)
+,
+(
+ 'Default_Folder'
+ ,'Expenses'
+ ,'Name used for Expenses folder in the root directory'
+)
+,
+(
+ 'Default_Folder'
+ ,'Income'
+ ,'Name used for Income folder in the root directory'
+)
+;
+;
--- Post_Deploy/Script.PostDeployment.sql
+++ Post_Deploy/Script.PostDeployment.sql
@@ -5,4 +5,9 @@
 --:r .\iterations\6.0\Insert_person_status.sql
 
 --7.0
-:r .\iterations\7.0\insert_Version.sql
+--:r .\iterations\7.0\insert_Version.sql
+
+--8.0
+:r .\iterations\8.0\insert_Version.sql
+:r .\iterations\8.0\insert_root_directory.sql
+:r .\iterations\8.0\modify-property-descriptons.sql
