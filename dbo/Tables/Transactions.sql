bfbe3477dfb2ad057b4f59f51f0dabdaa3d5cf76
Chad Shaver <c256s856@home.ku.edu>
2020-11-08 08:15:59 -0700

    Transaction table IDs and FKS

--- dbo/Tables/Transactions.sql
+++ dbo/Tables/Transactions.sql
@@ -1,6 +1,12 @@
 ﻿CREATE TABLE [dbo].[Transactions]
 (
-[Transaction_ID] INT NOT NULL PRIMARY KEY IDENTITY, 
-    [Transaction_Type_ID] INT NULL
+[Transaction_ID] INT NOT NULL PRIMARY KEY IDENTITY,
+[Property_Unit_ID] INT NULL,
+[Person_ID] INT NULL,
+[Transaction_Type_ID] INT NULL, 
+[Transaction_Category_ID] INT NOT NULL,
+[Transaction_Amount] MONEY NULL, 
+[Transaction_Date] DATE NULL, 
+    CONSTRAINT [FK_Transactions_ToTransition_Type] FOREIGN KEY ([Transaction_Type_ID]) REFERENCES [Transaction_Type]([Transaction_Type_ID])
 
 )
