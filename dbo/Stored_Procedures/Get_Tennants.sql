54de539b1666a515323270d60aadef7f268a4956
Chad Shaver <c256s856@home.ku.edu>
2020-11-10 14:27:14 -0700

    Adds stored procedure for get tennants

---
+++ dbo/Stored_Procedures/Get_Tennants.sql
@@ -0,0 +1,11 @@
+﻿CREATE PROCEDURE [dbo].[Get_Tennants]
+
+AS
+	SET NOCOUNT ON;
+
+SELECT        People.Person_Name_Combined, People.Person_ID
+FROM            People INNER JOIN
+                         Person_Role ON People.Person_ID = Person_Role.Person_ID INNER JOIN
+                         Person_Role_Type ON Person_Role.Person_Role_Type_ID = Person_Role_Type.Person_Role_Type_ID
+WHERE Person_Role_type = 'Tennant'
+;
