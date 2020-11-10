010d0679adb9665932266fb9f3491c7904e747a7
Chad Shaver <c256s856@home.ku.edu>
2020-11-10 14:49:37 -0700

    Modifed get person name procedure to only get tennant records

--- dbo/Stored_Procedures/Get_Person_Name.sql
+++ dbo/Stored_Procedures/Get_Person_Name.sql
@@ -2,14 +2,15 @@
 	
 AS
 	
-select
- Person_ID
- ,Person_Name_Combined
-FROM People
+SELECT        People.Person_Name_Combined, People.Person_ID
+FROM            People INNER JOIN
+                         Person_Role ON People.Person_ID = Person_Role.Person_ID INNER JOIN
+                         Person_Role_Type ON Person_Role.Person_Role_Type_ID = Person_Role_Type.Person_Role_Type_ID
+WHERE Person_Role_type = 'Tennant'
 UNION
 select
- 10000
- ,'Default, Tennant'
+ 'Default, Tennant'
+ ,100000
 FROM People 
 order by Person_Name_Combined
 ;
