495c7e92b9dedabb87d0ccf9ab2a23dad73eb960
Chad Shaver <c256s856@home.ku.edu>
2020-11-09 19:22:32 -0700

    Adds default value of getdate() for transaction dates

--- dbo/Tables/Transactions.sql
+++ dbo/Tables/Transactions.sql
@@ -6,11 +6,11 @@
 [Person_ID] INT NULL,
 [Transaction_Category_ID] INT NOT NULL,
 [Transaction_Amount] MONEY NULL, 
-[Transaction_Date] DATE NULL, 
+[Transaction_Date] DATE NULL DEFAULT GetDate(), 
 [Transaction_File_Path] VARCHAR(MAX) NULL, 
 [Transaction_Notes] VARCHAR(MAX) NULL, 
 [From_Security_Deposit] BIT NULL, 
-    [Service_Begin_Date] DATE NULL, 
-    [Service_End_Date] DATE NULL, 
+    [Service_Begin_Date] DATE NULL DEFAULT GetDate(), 
+    [Service_End_Date] DATE NULL DEFAULT GetDate(), 
     CONSTRAINT [FK_Transactions_ToTransactionCategory] FOREIGN KEY ([Transaction_Category_ID]) REFERENCES [Transaction_Category]([Transaction_Category_ID]) 
 )
