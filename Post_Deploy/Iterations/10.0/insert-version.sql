408f036a226059962a4bc900d0589b8c1f3acdf5
Chad Shaver <c256s856@home.ku.edu>
2020-11-12 11:53:44 -0700

    Adds version insert script

---
+++ Post_Deploy/Iterations/10.0/insert-version.sql
@@ -0,0 +1,17 @@
+﻿DELETE FROM tV
+FROM dbo.Version tV
+WHERE 
+	tV.number_Version = '10.0' 
+	
+
+INSERT INTO dbo.Version (
+	Number_Version
+	,Date_Version
+	,Desc_Version
+	)
+VALUES (
+	'10.0'
+	,GETDATE()
+	,'Transactions distributions'
+	)
+;
