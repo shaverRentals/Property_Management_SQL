d13cf4fa03d2d321fceddd06ecb63294a38de0e8
Chad Shaver <c256s856@home.ku.edu>
2020-11-12 11:51:19 -0700

    Adds property ID field to transaction table

--- Post_Deploy/Script.PostDeployment.sql
+++ Post_Deploy/Script.PostDeployment.sql
@@ -13,7 +13,10 @@
 --:r .\iterations\8.0\modify-property-descriptons.sql
 
 --9.0
-:r .\iterations\9.0\insert_Version.sql
-:r .\iterations\9.0\insert_transaction_types.sql
-:r .\iterations\9.0\insert_transaction_categories.sql
-:r .\iterations\9.0\updates-lease-description.sql
+--:r .\iterations\9.0\insert_Version.sql
+--:r .\iterations\9.0\insert_transaction_types.sql
+--:r .\iterations\9.0\insert_transaction_categories.sql
+--:r .\iterations\9.0\updates-lease-description.sql
+
+--10.0
+:r .\iterations\10.0\
--- dbo/Tables/Transactions.sql
+++ dbo/Tables/Transactions.sql
@@ -1,6 +1,7 @@
 ﻿CREATE TABLE [dbo].[Transactions]
 (
 [Transaction_ID] INT NOT NULL PRIMARY KEY IDENTITY,
+[Property_ID] INT NULL,
 [Property_Unit_ID] INT NULL,
 [Lease_ID] INT NULL,
 [Person_ID] INT NULL,
@@ -10,7 +11,7 @@
 [Transaction_File_Path] VARCHAR(MAX) NULL, 
 [Transaction_Notes] VARCHAR(MAX) NULL, 
 [From_Security_Deposit] BIT NULL, 
-    [Service_Begin_Date] DATE NULL DEFAULT GetDate(), 
-    [Service_End_Date] DATE NULL DEFAULT GetDate(), 
+[Service_Begin_Date] DATE NULL DEFAULT GetDate(), 
+[Service_End_Date] DATE NULL DEFAULT GetDate(), 
     CONSTRAINT [FK_Transactions_ToTransactionCategory] FOREIGN KEY ([Transaction_Category_ID]) REFERENCES [Transaction_Category]([Transaction_Category_ID]) 
 )
