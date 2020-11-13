31159790bd27c139ed60ac569f6aaaffb8464475
Chad Shaver <c256s856@home.ku.edu>
2020-11-13 07:27:56 -0700

    Adds YYYYMM field and historical data flag field to transactions table

--- dbo/Tables/Transactions.sql
+++ dbo/Tables/Transactions.sql
@@ -14,6 +14,8 @@
 [From_Security_Deposit] BIT NULL, 
 [Service_Begin_Date] DATE NULL DEFAULT GetDate(), 
 [Service_End_Date] DATE NULL DEFAULT GetDate(), 
+[Payment_Year_Month] INT NULL, 
+[Historical_Record] TINYINT NULL DEFAULT 0, 
     CONSTRAINT [FK_Transactions_ToTransactionCategory] FOREIGN KEY ([Transaction_Category_ID]) REFERENCES [Transaction_Category]([Transaction_Category_ID]) 
     ,
     CONSTRAINT [FK_Transactions_ToTransactionDistributionType] FOREIGN KEY ([Transaction_Distribution_ID]) REFERENCES [Transaction_Distribution_Type]([Transaction_Distribution_ID]) 
