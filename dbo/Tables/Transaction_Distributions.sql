d36f4f9e42c959d8f9bcc6081a560725c157033a
Chad Shaver <c256s856@home.ku.edu>
2020-11-13 10:51:24 -0700

    Creates transaction distributions table

---
+++ dbo/Tables/Transaction_Distributions.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Transaction_Distributions]
+(
+	[Transaction_Distribution_ID] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Transaction_ID] INT NOT NULL, 
+    [Lease_ID] INT NULL, 
+    [Transaction_Category_ID] INT NULL, 
+    [Transaction_Distribution_Type_ID] INT NULL, 
+    [Property_ID] INT NULL,
+    [Property_Unit_ID] INT NULL,
+    [Transaction_Amount] MONEY NULL, 
+    [Transaction_Distributed_Amount] MONEY NULL
+)
