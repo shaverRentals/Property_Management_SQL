53c659b985efab45c099cf84d3394fc5e67de167
Chad Shaver <c256s856@home.ku.edu>
2020-11-19 11:47:20 -0700

    Fix recon view

--- dbo/Views/Reconcilliations.sql
+++ dbo/Views/Reconcilliations.sql
@@ -19,10 +19,12 @@ TD.Lease_ID
 ,Transaction_Distributed_Amount
 ,Utility_Payment_Per_Day
 ,Adjusted_Utility_Payment
+,Transaction_Date
 FROM Transaction_Distributions TD
 INNER JOIN Leases L ON
 L.Lease_ID = TD.Lease_ID
 INNER JOIN Transaction_Category TC ON
 TC.Transaction_Category_ID = TD.Transaction_Category_ID
+where Recon_Flag = 1
 --order by Transaction_Distribution_Service_Begin_Month
 ;
