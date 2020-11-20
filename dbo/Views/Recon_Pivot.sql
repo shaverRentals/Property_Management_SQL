e5ddc4f3b449025374b0ae3760a57cc9e1858858
Chad Shaver <c256s856@home.ku.edu>
2020-11-20 08:02:06 -0700

    Adds recon pivot view for recon reports

---
+++ dbo/Views/Recon_Pivot.sql
@@ -0,0 +1,24 @@
+﻿CREATE VIEW [dbo].[Reconcilliations]
+	AS 
+
+	
+select Lease_ID
+		,Lease_Description
+	   , Transaction_Distribution_Payment_Month
+	   ,Lease_Begin_Date
+	   ,Lease_End_Date
+	   ,SUM(Utilities_Paid) Utility_Rent
+	   ,SUM(City) City
+	   ,SUM(Gas) Gas
+	   ,SUM(Power) Power
+	   ,SUM(Total_Utilites) Total_Utilities
+	   ,SUM(Difference) Diff
+	   from Recon_Pivot
+	   GROUP BY
+	  Lease_ID
+		,Lease_Description
+	   , Transaction_Distribution_Payment_Month
+	    ,Lease_Begin_Date
+	   ,Lease_End_Date
+	   
+	   ;
