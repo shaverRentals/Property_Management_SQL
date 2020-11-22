f4c6e69f749857b6e3b4a20c41bdc1a125b5af07
Chad Shaver <c256s856@home.ku.edu>
2020-11-22 09:26:38 -0700

    Sets up release 13.0

---
+++ Post_Deploy/Iterations/13.0/inserts_version.sql
@@ -0,0 +1,17 @@
+﻿DELETE FROM tV
+FROM dbo.Version tV
+WHERE 
+	tV.number_Version = '13.0' 
+	
+
+INSERT INTO dbo.Version (
+	Number_Version
+	,Date_Version
+	,Desc_Version
+	)
+VALUES (
+	'13.0'
+	,GETDATE()
+	,'Clean up: Production instance'
+	)
+;
