57dedd434ef1ded7a62f2735299c416bb03867ea
Chad Shaver <c256s856@home.ku.edu>
2021-01-09 13:30:46 -0700

    Create release 15.0

--- Post_Deploy/Iterations/15.0/insert_version.sql
+++ Post_Deploy/Iterations/15.0/insert_version.sql
@@ -1,1 +1,16 @@
-﻿
+﻿DELETE FROM tV
+FROM dbo.Version tV
+WHERE 
+	tV.number_Version = '15.0' 
+	
+
+INSERT INTO dbo.Version (
+	Number_Version
+	,Date_Version
+	,Desc_Version
+	)
+VALUES (
+	'15.0'
+	,GETDATE()
+	,'Fix transactions filters when updating'
+;
