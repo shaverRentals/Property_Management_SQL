6f0271520ecd72e553302b4bd9f26bd29746d1af
Chad Shaver <c256s856@home.ku.edu>
2021-08-06 07:26:56 -0600

    Adds IRS transaction categories to Transaction_Categories table

--- dbo/Tables/Transaction_Category.sql
+++ dbo/Tables/Transaction_Category.sql
@@ -2,8 +2,8 @@
 (
 	[Transaction_Category_ID] INT NOT NULL PRIMARY KEY IDENTITY, 
     [Transaction_Type_ID] INT NULL, 
-
     [Transaction_Category] VARCHAR(50) NULL, 
+    [Transaction_Category_IRS] VARCHAR(50) NULL, 
     CONSTRAINT [FK_Transactions_ToTransition_Type] FOREIGN KEY ([Transaction_Type_ID]) REFERENCES [Transaction_Type]([Transaction_Type_ID])
 
 )
