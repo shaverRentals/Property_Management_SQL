8d8349ac1fc5e01b75b7b5e42ec1fc3d02cec5ee
Chad Shaver <c256s856@home.ku.edu>
2021-08-06 07:54:30 -0600

    Creates IRS income tax table

---
+++ dbo/Tables/Income_Tax_IRS.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Income_Tax_IRS]
+(
+	[ID] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Property_Name] VARCHAR(50) NULL, 
+    [Property_Owner_Name] VARCHAR(50) NULL,
+    [Transaction_Category_IRS] VARCHAR(50) NULL, 
+    [Transaction_Amount] MONEY NULL, 
+    [Transaction_Payment_Year_IRS] INT,
+)
