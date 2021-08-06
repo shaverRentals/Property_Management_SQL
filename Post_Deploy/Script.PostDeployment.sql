58678dd6fece6209839931f708cfa321fb292580
Chad Shaver <c256s856@home.ku.edu>
2021-08-06 09:24:18 -0600

    fix transaction distriution values for non utilites for 358 8th street units

--- Post_Deploy/Script.PostDeployment.sql
+++ Post_Deploy/Script.PostDeployment.sql
@@ -63,7 +63,7 @@
 --20.0
 :r .\iterations\21.0\insert_version.sql
 :r .\iterations\21.0\update-irs-transaction-categories.sql
-
+:r .\iterations\21.0\fix-transaction-distribution-values-358-8th.sql
 
 
 
