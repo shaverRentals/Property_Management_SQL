a45e79455fce23bfd11bd32bb9d5fdde5e4aaa19
Chad Shaver <c256s856@home.ku.edu>
2021-08-06 08:55:06 -0600

    Adds ID and transaction date fields to IRS Table

--- dbo/Tables/Income_Tax_IRS.sql
+++ dbo/Tables/Income_Tax_IRS.sql
@@ -1,6 +1,8 @@
 ﻿CREATE TABLE [dbo].[Income_Tax_IRS]
 (
 	[ID] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Transaction_Distribution_ID] INT,
+    [Transaction_Date] DATE NULL, 
     [Property_Name] VARCHAR(50) NULL, 
     [Property_Owner_Name] VARCHAR(50) NULL,
     [Transaction_Category_IRS] VARCHAR(50) NULL, 
